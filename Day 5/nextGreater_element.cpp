b03280d1a9406e9e418444cbbb5ebee039cab007
Omkar Sankannavar <omkarsankannavar221@gmail.com>
2025-08-19 22:40:43 +0530

    Create nextGreater_element.cpp

---
+++ Day 5/nextGreater_element.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+vector<int> findLeaders(vector<int>& arr) {
+    int n = arr.size();
+    vector<int> leaders;
+    
+    int maxFromRight = arr[n-1]; 
+    leaders.push_back(maxFromRight);  
+    
+    for (int i = n-2; i >= 0; i--) {
+        if (arr[i] >= maxFromRight) {
+            leaders.push_back(arr[i]);
+            maxFromRight = arr[i];
+        }
+    }
+    
+    reverse(leaders.begin(), leaders.end());
+    return leaders;
+}
+
+int main() {
+    vector<int> arr = {16, 17, 4, 3, 5, 2};
+    vector<int> result = findLeaders(arr);
+
+    for (int x : result) 
+    {
+        cout << x << " ";
+    }
+    
+}
