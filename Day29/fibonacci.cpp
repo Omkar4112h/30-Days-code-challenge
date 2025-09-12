69c9ea19488b38e09ef9dbcb39a245ceaa594e56
Omkar Sankannavar <omkarsankannavar221@gmail.com>
2025-09-12 21:57:02 +0530

    Create fibonacci.cpp

---
+++ Day29/fibonacci.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+long long fibonacci(int n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+
+    long long prev2 = 0, prev1 = 1, curr;
+    for (int i = 2; i <= n; i++) {
+        curr = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = curr;
+    }
+    return prev1;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << fibonacci(n) << endl;
+    return 0;
+}
