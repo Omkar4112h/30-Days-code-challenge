ed6504f6f1b906e4b89e0107162afe4e9752c8e5
Omkar Sankannavar <omkarsankannavar221@gmail.com>
2025-09-13 22:41:02 +0530

    Create  coinChange.cpp

---
+++ Day30/coinChange.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int coinChange(vector<int>& coins, int amount) {
+    vector<int> dp(amount + 1, amount + 1);
+    dp[0] = 0;
+
+    for (int i = 1; i <= amount; i++) {
+        for (int c : coins) {
+            if (c <= i)
+                dp[i] = min(dp[i], dp[i - c] + 1);
+        }
+    }
+    return dp[amount] > amount ? -1 : dp[amount];
+}
+
+int main() {
+    vector<int> coins = {1, 2, 5};
+    int amount = 11;
+    cout << coinChange(coins, amount);
+    return 0;
+}
